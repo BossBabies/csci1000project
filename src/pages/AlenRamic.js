d1ee8f7415308ecbd04f9df0545b3e73ee9fe424
Alen Ramic <alra2023@colorado.edu>
2022-11-14 17:23:29 -0700

    alen commit

---
+++ src/pages/AlenRamic.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <main>
+            <h1
+                classname ='title'> Hi my name is Alen Ramic
+            </h1>
+            <p>
+                This is my website
+            </p>
+
+
+        </main>
+    );
+}
