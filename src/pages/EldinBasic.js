bca20e789d6cde853f19f0cd0fd9fb46d57d03b4
Eldin Basic <112306910+elba8705@users.noreply.github.com>
2022-11-15 21:36:16 -0700

    Add files via upload

---
+++ src/pages/EldinBasic.js
@@ -0,0 +1,44 @@
+
+export default function Home() {
+    return (
+    <div id="container">
+        <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Eldin Basic | About Me</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
+        </head>
+        <main>
+            <div class="alert alert-warning" align="center">
+                You are now viewing: Deen's webpage
+            </div>
+            <img className = 'picture'
+            src = 'https://pbs.twimg.com/profile_images/1563020466721132544/LdBd6sCy_400x400.jpg'
+            alt = 'Picture of me'
+            align = "right"
+            hspace = "20"
+            vspace = "20"
+            />
+            
+
+            <h1 id="header">Eldin Basic </h1>
+            <h7> Eldin Basic (born December 10, 2003) is a programmer, tennis player, and current
+                student at CU Boulder. 
+                <br></br>
+                <br></br>
+                He is a first year computer science major. Basic has shown great interest in the field and has high aspirations for it.
+                Going to Overland High School, he took various computer science AP classes as well as cybersecurity courses on the side.
+                Basic has certification in certain security protocol tests in Linux and CompTIA.
+                <br></br>
+                <br></br>
+                Basic is from Bosnia and Herzegovina. With a brother born in Germany, he has family across the globe. </h7>
+            
+           
+        
+        
+        </main>
+        </div>
+
+    );
+
+}
