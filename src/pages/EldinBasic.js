3a77f0cd6172cd0005e9892cad146bdb3fdd0a1c
Eldin Basic <112306910+elba8705@users.noreply.github.com>
2022-11-15 21:37:10 -0700

    Update EldinBasic.js

--- src/pages/EldinBasic.js
+++ src/pages/EldinBasic.js
@@ -9,7 +9,7 @@ export default function Home() {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
         </head>
         <main>
-            <div class="alert alert-warning" align="center">
+            <div class="yellow-header" align="center">
                 You are now viewing: Deen's webpage
             </div>
             <img className = 'picture'
