0614114d55be42458f8bb5aa18c2a7c8b3bdccdc
Eldin Basic <112306910+elba8705@users.noreply.github.com>
2022-11-17 23:53:13 -0700

    final touches on website

--- src/pages/EldinBasic.js
+++ src/pages/EldinBasic.js
@@ -1,4 +1,5 @@
 
+
 export default function Home() {
     return (
     <div id="container">
@@ -9,7 +10,7 @@ export default function Home() {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
         </head>
         <main>
-            <div class="yellow-header" align="center">
+            <div class="alert alert-warning" align="center">
                 You are now viewing: Deen's webpage
             </div>
             <img className = 'picture'
@@ -22,8 +23,8 @@ export default function Home() {
             
 
             <h1 id="header">Eldin Basic </h1>
-            <h7> Eldin Basic (born December 10, 2003) is a programmer, tennis player, and current
-                student at CU Boulder. 
+            <h7><strong>Eldin Basic </strong>(born December 10, 2003) is a programmer, tennis player, and current
+                student at CU Boulder. Basic is often referred to as simply "Deen".
                 <br></br>
                 <br></br>
                 He is a first year computer science major. Basic has shown great interest in the field and has high aspirations for it.
